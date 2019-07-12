8003562637ffeed4b132b3c824715d7779c422db
Surya <37186895+SuryaBalasubramaniyan@users.noreply.github.com>
2019-07-12 13:59:57 +0530

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+
+
+public class Abc{
+		public static void main(String[] args){
+			Class c1=Class.forName("LambdaDemo$1");
+			Object obj=c1.newInstance();
+					System.out.println(obj);
+					
+		}
+
+
+}
--- LamdaDemo.java
+++ LamdaDemo.java
@@ -4,15 +4,28 @@ interface I{
 	public void abc();
 }
 
-
+interface J{
+	public void xyz(String msg);
+}
+interface K{
+	public int add(int a,int b);
+}
 public class LamdaDemo{
 		public static void main(String[] args){
-			I i1= new I(){
+			/*I i1= new I(){
 				public void abc(){
 					System.out.println("From anonymous class");
 					}
-				};
+				};*/
+			I i1=()->System.out.println("From Lambda function");
 			i1.abc();
+
+			J j1=(msg)->System.out.println("Got the message :"+msg);
+			j1.xyz("Hi");
+
+			K k1=(a,b)->a+b; 
+			int res=k1.add(1,4);
+ 			System.out.println("Result :"+res);
 		}
 
 
